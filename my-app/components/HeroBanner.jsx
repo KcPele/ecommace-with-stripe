ab39bdd1e1347aaa06c05f359f04d1944454d169
Kc pele <fidekg123@gmail.com>
2022-05-20 16:46:33 +0000

    final deploying

--- my-app/components/HeroBanner.jsx
+++ my-app/components/HeroBanner.jsx
@@ -5,7 +5,7 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beat-solo">{heroBanner.smallText}</p>
+        <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
         <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
